e891c56bf746633678cbb7763e26d93ad579888a
HuangHangHang <43408457+HuangHangHang@users.noreply.github.com>
2018-10-02 00:55:35 +0800

    Update IndexController.class.php

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -4,19 +4,13 @@ use Think\Controller;
 
 class IndexController extends Controller{
     public function index(){
-        //左侧高级分类和中级分类
-        $left_array  = $this->get_left_info();
-        //右侧中级分类和数据信息
-        $right_array = $this->get_right_info();
-        //热门网站
-        $hot_array = $this->get_hot_info();
-        //广告信息
-        $ad_array  = $this->get_hot_info($type = 1);
-        //图片区
-        $pic_array = $this->get_hot_info($type = 2);
-        //底部高级分类和数据信息
-        $bottom_array = $this->get_bottom_info();
-
+        $left_array  = $this->get_left_info();   //左侧高级分类和中级分类      
+        $right_array = $this->get_right_info();    //右侧中级分类和数据信息      
+        $hot_array = $this->get_hot_info();   //热门网站     
+        $ad_array  = $this->get_hot_info($type = 1);   //广告信息    
+        $pic_array = $this->get_hot_info($type = 2);  //图片区     
+        $bottom_array = $this->get_bottom_info();  //底部高级分类和数据信息
+        
         $this->assign('left_array' ,$left_array);
         $this->assign('right_array',$right_array);
         $this->assign('hot_array'  ,$hot_array);
@@ -27,18 +21,15 @@ class IndexController extends Controller{
     }
 
     /**
-     * 根据中级分类id,更多数据信息
+     * 根据中级分类id,获取更多数据信息
      */
     public function more(){
-        $map['middle_id'] = I('middle_id',0,'int'); //接受中级分类id
+        $map['middle_id'] = I('middle_id',0,'int');  //接收中级分类id
         $elementary_level = M('elementary_level')->where($map)->select();   //查找初级分类
-        foreach($elementary_level as $elementary){
-            //初级分类数据
-            $array = M('datalist')->where(array('elementary_id'=>$elementary['id']))->select();
-            //初级分类下的数量
-            $count = M('datalist')->where(array('elementary_id'=>$elementary['id']))->count();
-            //组装成数组
-            $key = $elementary['elementary_name'].'('.$count.')';
+        foreach($elementary_level as $elementary){           
+            $array = M('datalist')->where(array('elementary_id'=>$elementary['id']))->select();  //初级分类数据          
+            $count = M('datalist')->where(array('elementary_id'=>$elementary['id']))->count();  //初级分类下的数量       
+            $key = $elementary['elementary_name'].'('.$count.')';   //组装成数组
             $datalist[$key] = $array;
         }
         $more_info = $datalist;
@@ -48,13 +39,14 @@ class IndexController extends Controller{
 
     /**
      * 获取左部信息
-     * @return array:以中级分类为键值的数组
      */
     private function get_left_info(){
         $map['is_display'] = 1;         // 1显示，0关闭
         $map['layout']     = 'left';    //输出左侧数据
-        $high_level = M('high_level')->where($map)->order('sort asc')->select();//获取高级分类
+        $high_level = M('high_level')->where($map)->order('sort asc')->select(); //获取高级分类
+        
         /**获取高级分类下的初级分类**/
+        
         foreach($high_level as $high){
             $array = M('middle_level')->where(array('high_id'=>$high['id']))->select();
             $middle_array[$high['high_name']] = $array;
@@ -75,7 +67,7 @@ class IndexController extends Controller{
     }
 
     /**
-     * 获取热门网址 or 广告位 or 图片区
+     * 获取热门网址 或 广告位 或 图片区
      * @param int $type 0：热门 1：广告 2：图片
      * @return array ： 数组信息
      */
